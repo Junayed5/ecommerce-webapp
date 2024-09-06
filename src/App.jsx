3396302072ac8ba2a17ab05331b6c456d663558d
Junayed5 <mdjunayed352003@gmail.com>
2024-09-06 12:42:26 +0600

    Registration Page created

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Cart from "./pages/Cart";
+import Registration from "./pages/Registration";
 
 function App() {
   return (
     <>
-      
+      <Registration/>
     </>
   );
 }
